a9311260d2b9842069720a70bf731a09a1c6ba9e
satyam <tech.kumarsatyam@gmail.com>
2022-09-01 18:39:36 +0530

    More QS Added

---
+++ POINTER/Charpointer.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{   //QS->1
+/*  char arr[20];
+    int i = 0;
+    for (i = 0; i < 10; i++)
+    {
+        *(arr + i) = 65 + i;
+    }
+    *(arr + i) = '\0';
+    cout << arr; */
+
+//QS->2
+    /*char *ptr;
+    char str[] = "abcdefg";
+    ptr = str;
+    ptr +=5;
+    cout<<ptr; */
+
+/*QS->3
+    /int num[5];
+    int *p;
+    p=num;
+    *p = 10;
+    p =&num[2];
+    *p = 20;
+    p--;
+    *p = 30;
+    p = num +3;
+    *p = 40;
+    p  =num;
+    *(p+4) = 50;
+    for(int n = 0; n<5;n++){
+        cout<<num[n]<<", ";
+    } */
+
+    //QS->4
+    /*char st[]= "ABCD";
+    for(int i = 0; st[i] != '\0'; i++){
+        cout<<st[i]<<*(st)+i << *(i+st)<<i[st];
+    }*/
+
+
+/*QS->5
+    float arr[5] = {12.5,10.0,13.5,90.5,0.5};
+    float *ptr1 = &arr[0];
+    float *ptr2 = ptr1 +3;
+    cout<<*ptr2<<endl;
+    cout<<ptr2 - ptr1;*/
+
+
+    
+    return 0;
+}
--- POINTER/PointerWithFunc.cpp
+++ POINTER/PointerWithFunc.cpp
@@ -29,3 +29,26 @@ int main()
     int arr[5] = {1,2,3,4,5};
     cout<<getsum(arr,5)<<endl;
 }
+
+/*QS--2
+    void fun(int a[]){
+        cout<<a[0]<< " ";
+    }
+    int main(){
+        int a[] = {1,2,3,4};
+        fun(a+1);
+        cout<<a[0];
+    } */
+
+/*  QS- 3
+void square(int *p)
+{
+    int a = 10;
+    p = &a;
+    *p = (*p) * (*p);
+}
+int main(){
+    int a = 10;
+    square(&a);
+    cout<<a<<endl;
+}*/
